a33e0605e90eccefb4185f94a3ee64b1fc500227
lanners-marshall <lanners.marshall@gmail.com>
2019-10-27 14:44:29 -0700

    setup props default props

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,15 @@
+import checkProptypes from 'check-prop-types';
+
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkProptypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
+  expect(propError).toBeUndefined();
 };
