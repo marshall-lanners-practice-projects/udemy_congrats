415f481ce1914121284c39febd2addb6c33e8fa0
lanners-marshall <lanners.marshall@gmail.com>
2019-10-27 12:57:09 -0700

    set up congrats component and tests

---
+++ src/Congrats.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Congrats = props => {
+  return (
+    <>
+      {props.success ? (
+        <div data-test='component-congrats'>
+          <span data-test='congrats-message'>
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+      ) : (
+        <div data-test='component-congrats'></div>
+      )}
+    </>
+  );
+};
+
+export default Congrats;
